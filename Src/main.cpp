be71bb798cf0930b88029e736df55ee3faa4ae61
tn_mai <rabitfiver@gmail.com>
2016-03-17 19:17:28 +0900

    Fix some error.

--- Src/main.cpp
+++ Src/main.cpp
@@ -2,18 +2,20 @@
 */
 #include <ImfRgbaFile.h>
 #include <ImfArray.h>
+#include <png.h>
 #include <iostream>
 #include <algorithm>
 #include <memory>
+#include <vector>
 
-enum class Result {
-  Success = 0;
-  Error = 1;
+enum Result {
+  RESULT_SUCCESS = 0,
+  RESULT_ERROR = 1,
 };
 
 void ReadRgbaExrFile(const char* filename, Imf::Array2D<Imf::Rgba>* pPixels, int* pWidth, int* pHeight) {
-  Imf::RgbaInputFile file (fileName);
-  Imf::Box2i dw = file.dataWindow();
+  Imf::RgbaInputFile file (filename);
+  Imath::Box2i dw = file.dataWindow();
 
   if (pWidth) {
     *pWidth  = dw.max.x - dw.min.x + 1;
@@ -22,8 +24,8 @@ void ReadRgbaExrFile(const char* filename, Imf::Array2D<Imf::Rgba>* pPixels, int
     *pHeight = dw.max.y - dw.min.y + 1;
   }
   if (pPixels) {
-    pPixels->resizeErase (height, width);
-    file.setFrameBuffer(&(*pPixels)[0][0] - dw.min.x - dw.min.y * width, 1, width);
+    pPixels->resizeErase (*pHeight, *pWidth);
+    file.setFrameBuffer(&(*pPixels)[0][0] - dw.min.x - dw.min.y * *pWidth, 1, *pWidth);
     file.readPixels(dw.min.y, dw.max.y);
   }
 }
@@ -43,10 +45,10 @@ void SetPixel(png_bytep buf, const Imf::Rgba& rgba) {
     p[3] = 255;
   } else {
     const float strength = biggest / 255.0f;
-    p[0] = std::min(255, static_cast<uint8_t>(rgba.r / strength));
-    p[1] = std::min(255, static_cast<uint8_t>(rgba.g / strength));
-    p[2] = std::min(255, static_cast<uint8_t>(rgba.b / strength));
-    p[3] = std::max(1, static_cast<uint8_t>(255.0f / strength));
+    p[0] = std::min<uint8_t>(255, static_cast<uint8_t>(rgba.r / strength));
+    p[1] = std::min<uint8_t>(255, static_cast<uint8_t>(rgba.g / strength));
+    p[2] = std::min<uint8_t>(255, static_cast<uint8_t>(rgba.b / strength));
+    p[3] = std::max<uint8_t>(1, static_cast<uint8_t>(255.0f / strength));
   }
 }
 
@@ -63,7 +65,7 @@ int main(int argc, char** argv) {
   }
   if (!infilename) {
     PrintUsage();
-    return Result::Success;
+    return RESULT_SUCCESS;
   }
   if (outfilename.empty()) {
     outfilename = infilename;
@@ -77,23 +79,23 @@ int main(int argc, char** argv) {
   int w, h;
   ReadRgbaExrFile(infilename, &pixels, &w, &h);
 
-  auto del = [](png_structp p){ png_destroy_write_struct(&p, nullptr); }
+  auto del = [](png_structp p) { png_destroy_write_struct(&p, nullptr); };
   std::unique_ptr<png_struct, decltype(del)> pngWriter(png_create_write_struct(PNG_LIBPNG_VER_STRING, nullptr, nullptr, nullptr), del);
   if (!pngWriter) {
-    return Result::Error;
+    return RESULT_ERROR;
   }
-  png_infop pngInfo = png_create_info_struct(pngWriter);
+  png_infop pngInfo = png_create_info_struct(pngWriter.get());
   if (!pngInfo) {
-    return Result::Error;
+    return RESULT_ERROR;
   }
 
   std::unique_ptr<FILE, decltype(&fclose)> fp(fopen(outfilename.c_str(), "wb"), &fclose);
   if (!fp) {
-    return Result::Error;
+    return RESULT_ERROR;
   }
 
-  if (setjmp(png_impbuf(pngWriter.get()))) {
-    return Result::Error;
+  if (setjmp(png_jmpbuf(pngWriter.get()))) {
+    return RESULT_ERROR;
   }
   png_init_io(pngWriter.get(), fp.get());
   png_set_IHDR(pngWriter.get(), pngInfo, w, h, 8, PNG_COLOR_TYPE_RGB_ALPHA, PNG_INTERLACE_NONE, PNG_COMPRESSION_TYPE_DEFAULT, PNG_FILTER_TYPE_DEFAULT);
@@ -102,10 +104,10 @@ int main(int argc, char** argv) {
   row.resize(4/*8bit RGBA*/ * w * sizeof(png_byte));
   for (int y = 0; y < h; ++y) {
     for (int x = 0; x < w; ++x) {
-      setPixel(row[x * 4], pixels[y][x]);
+      SetPixel(&row[x * 4], pixels[y][x]);
     }
     png_write_row(pngWriter.get(), row.data());
   }
 
-  return Result::Success;
+  return RESULT_SUCCESS;
 }
